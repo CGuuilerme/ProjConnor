01d7ec1c917c5563cb5b9a5223c48c47de1975da
CGuuilerme <coelhoguilherme315@gmail.com>
2026-08-27 16:20:52 -0300

    Primeira Versão do Projeto Connor - V0.1

---
+++ Back-End.py
@@ -0,0 +1,10 @@
+#começo de quando futuramente o aplicativo for feito
+print("Olá, bom dia!")
+
+# Vconta = Valor em conta
+vconta = float(input("Digite o valor que está na sua conta: "))
+# Vadicionar = valor que vai adicionar
+vadicionar = float(input("Digite o valor que você quer adicionar: "))
+# vconta2 = valor que vai estar na conta após a adição do valor
+vconta2 = (vconta + vadicionar)
+print("O valor que está na sua conta agora é:", vconta2)
