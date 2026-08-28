b0fccae2a01aaf69bf4e506b951b09fe79e587be
CGuuilerme <coelhoguilherme315@gmail.com>
2026-08-27 22:03:00 -0300

    Adicionado menu e registro de despesa

--- Back-End.py
+++ Back-End.py
@@ -1,10 +1,34 @@
 #começo de quando futuramente o aplicativo for feito
 print("Olá, bom dia!")
 
-# Vconta = Valor em conta
-vconta = float(input("Digite o valor que está na sua conta: "))
-# Vadicionar = valor que vai adicionar
-vadicionar = float(input("Digite o valor que você quer adicionar: "))
-# vconta2 = valor que vai estar na conta após a adição do valor
-vconta2 = (vconta + vadicionar)
-print("O valor que está na sua conta agora é:", vconta2)
+saldo = 0
+
+continuar = True
+
+while continuar:
+
+    print("1 - Adicionar saldo\n2 - Registrar despesas\n3 - ver saldo")
+
+    N = int(input("Escolha um número: "))
+
+    if N == 1:
+        saldo_adicionado = float(input("Digite o saldo que deseja adicionar: "))
+        print("Saldo adicionado.")
+        saldo = saldo + saldo_adicionado
+
+    elif N == 2:
+        despesa = float(input("Digite o valor da despesa: "))
+        if saldo >= despesa:
+            saldo = saldo - despesa
+            print("Despesa registrada com sucesso!")
+        else:
+            print("Saldo insuficiente para suprir despesa!")
+    elif N == 3:
+        print(saldo)
+
+    else:
+        print("opcao inválida")
+    
+    opcao = input("Deseja selecionar outro número? [S/N]")
+    if opcao != 'S':
+        continuar = False
